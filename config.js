43326c5406ede1dc9531b7ac8d29e98a99aeee73
kapuluta2 <167614820+kapuluta1@users.noreply.github.com>
2024-07-06 00:28:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaDK8ZUDj
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/fee7af11b4edf8eb7c272.jpg";
 global.devs = "923135673658";
 global.sudo = process.env.SUDO || "923135673658,923192173398";
-global.owner = process.env.OWNER_NUMBER || "923135673658";
+global.owner = process.env.OWNER_NUMBER || "+255674477908";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,7 +42,7 @@ global.scan = "https://wasimdscanner-68feefafb737.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "   eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkdwUFZZL202WXhwQkw3TzhoOWlrM0pldkgzTVNJRnd6RTVrTlJKOHkxbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOUE0RDlSeWxqSU03LzlaVXBiTWxrcFVFV1Z0VEdDeTdWMzdtcXlxcUlROD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJXSll0K0l3enNLYnJ2SXZLRjMybVZxS0g0cGRRWm4rM1JJUjFFckNRSm5ZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJqM2NrUWpUMm5HWTVzRmxPdXl3SnN5dHkyaERxWGwrUzg4YXo5MDhLQjNBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBOZHpwdkFKY3RHSDlrYkh2SjlVelF1TEQzYklaSEIyOWtqTVoxaW41VXc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImRLWk1rU3pvSDNrcTRkZ0J6QWRYSEZZcGNuUkdxdnFHZEpGTXZheStXRGc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0lIN0taRWx2RVdIMU0wR092SkdmeXZZc3dWTDBjRk1JWFNDYVV1dS9Ycz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieFBCeXZIa3BoQ1dUeFBlekhyb1Eyazd2WFNXY05FRUdyNk1hODMxZ0gwMD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjVmeFhEZERpc2d3M1FTMloraVlscyttMStZZ3JwTWlUODBIaUNVNnozYkJmN0REWTl5cHAvWVVpaHZTaWRXdkJHalI3cFBnUGpMNGFmMUJXQUx0ckFnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTc4LCJhZHZTZWNyZXRLZXkiOiJhR2ZtSDhEdysrQnlERnZKSGU5NU5VTlZFeHpUdnZNcEhzcnRlemtHWnh3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6NjEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjo2MSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJIbWpLZVYxOFQ5V1dPWkwwVnkwR25BIiwicGhvbmVJZCI6IjBiMzAwZmQ5LWE5OGQtNDFkZS1hNzI4LTIzM2U1YWM1NDE4NiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ6WmhiclZXd2FHTnZSWDhZN0gyUTYyQWFqK1U9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaUw4TUN1MG1OYWM1cDdJRnZGay9Ed0UwUFpNPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkRRNTZGNkNMIiwibWUiOnsiaWQiOiIyNTU2NzQ0Nzc5MDg6MzlAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ04yTnpma0ZFS25Jb2JRR0dBUWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IjZ1VU5PRS9icmxpNkhsUW9qcmpKcFNGcnhGYngzakQwSUpBVThyWGorU2M9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkxxTmYyQXc0MHlxWmVXcWRhSEFMbW5yRnVreHpzZFg0ekgwWlU1TElnUUxxdmxQSFBzcnNpdmVYZktTTmRnb1dsdUowYkxGelFsTWxKaWhmZmk5bUF3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJLV0N2djl3QUVHT1B1cWJCVUFzTVBpSkRrZXJPYm9JaDZZSlJyYWxKVm5nQXB4eElwRlVHSFlFSXlhb00wa2dOZ09YdGxVdWwySkg5WlNmd0s2anhEZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NTY3NDQ3NzkwODozOUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJlcmxEVGhQMjY1WXVoNVVLSTY0eWFVaGE4Ulc4ZDR3OUNDUUZQSzE0L2tuIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwMjE0NTg0LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQU1PVCJ9 "
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "WASO",
   packname: process.env.PACK_NAME || "♥️",
   botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
-  ownername: process.env.OWNER_NAME || "WASI",
+  ownername: process.env.OWNER_NAME || "𝔹𝔸𝔹𝔸 𝕊𝕌ℂℂ𝔼𝕊𝕊",
   errorChat: process.env.ERROR_CHAT || "923192173398",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
